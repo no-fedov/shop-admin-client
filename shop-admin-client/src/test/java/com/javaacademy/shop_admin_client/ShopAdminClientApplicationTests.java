beb1415fcc2ccf118be938aa81ec8703ad3ae988
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-12-16 12:34:20 +0300

    init commit

---
+++ shop-admin-client/src/test/java/com/javaacademy/shop_admin_client/ShopAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaacademy.shop_admin_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
